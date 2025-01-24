0233a3f2c0b0556a280e3ee167d6c589a5bb79c0
2blackhole <barabanegor2000@gmail.com>
2025-01-24 23:02:55 +0300

    создали табличку с каналами и сообщениями в базе данных, пока никак с ними не получится взаимодействовать

--- backend/database/database.js
+++ backend/database/database.js
@@ -21,6 +21,27 @@ let db = new sqlite3.Database(dbname, (err) => {
             if (err) console.error(err.message);
             else {console.log("Table user_server created / already exists");}
         })
+
+        db.run("CREATE TABLE IF NOT EXISTS text_channels " +
+            "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "server_id INTEGER NOT NULL," +
+            "name VARCHAR(50))", err => {
+            if (err) console.error(err.message);
+            else {console.log("Table text_channels created / already exists");}
+        })
+
+        db.run("CREATE TABLE IF NOT EXISTS messages " +
+            "(id INTEGER PRIMARY KEY AUTOINCREMENT," +
+            "channel_id INTEGER NOT NULL," +
+            "type INTEGER, " +
+            "content BLOB," +
+            "text TEXT(5000)," +
+            "time_stamp INTEGER NOT NULL," +
+            "time_stamp_edited INTEGER" +
+            "visible INTEGER)", err => {
+            if (err) console.error(err.message);
+            else {console.log("Table messages created / already exists");}
+        })
     }
 });
 
