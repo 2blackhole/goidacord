cd0c80864cab3033faa351767070e916cbcb2ec3
Dmitry <dmgalochkin@gmail.com>
2025-02-04 17:00:21 +0300

    users update

--- backend/database/db_users.js
+++ backend/database/db_users.js
@@ -3,42 +3,44 @@ const db = require('./database')
 const createItem = (login, password, email, callback) => {
     const sql = `INSERT INTO users (login, password, email) VALUES (?, ?, ?)`
     db.run(sql, [login, password, email], function (err) {
-        callback(err, {lastID: this.lastID})
+        callback(err, {lastID: this.lastID});
     })
 }
 
-const readItems = (id, callback) => {
+const readItem = (id, callback) => {
     const sql = `SELECT * FROM users WHERE id = ?`;
-    db.all(sql, [id], callback)
+    db.get(sql, id, callback);
 }
 
 const updateItem = (id, login, password, email, callback) => {
     const sql = `UPDATE users SET login = ?, password = ?, email = ? WHERE id = ?`;
-    db.run(sql, [login, password, email, id], callback)
+    db.run(sql, [login, password, email, id], callback);
 }
 
 const deleteItem = (id, callback) => {
     const sql = `DELETE FROM users WHERE id = ?`;
-    db.run(sql, id, callback)
+    db.run(sql, id, callback);
 }
 
 const checkItemLogin = (login, callback) => {
     const sql = `SELECT * FROM users WHERE login = ?`;
-    db.get(sql, login, function (err, result) {
-        callback(err, result);
-    })
+    db.get(sql, login, callback);
 }
 
 const checkItemEmail = (email, callback) => {
     const sql = `SELECT * FROM users WHERE email = ?`;
-    db.get(sql, email, function (err, result) {
-        callback(err, result)
-    })
+    db.get(sql, email, callback)
+}
+
+const getUserInfoById = (id, callback) => {
+    const sql = `SELECT id, login FROM users WHERE id = ?`;
+    db.get(sql, id, callback)
 }
 
 exports.createItem = createItem;
-exports.readItems = readItems;
+exports.readItem = readItem;
 exports.updateItem = updateItem;
 exports.deleteItem = deleteItem;
 exports.checkItemLogin = checkItemLogin;
 exports.checkItemEmail = checkItemEmail;
+exports.getUserInfoById = getUserInfoById;
--- backend/handlers/users/users.js
+++ backend/handlers/users/users.js
@@ -1,10 +1,11 @@
 const db_users = require('../../database/db_users');
 
 module.exports.getUserInfo = (req, res) => {
-    db_users.readItems(req.id, (err, result) => {
+    db_users.getUserInfoById(req.id, (err, result) => {
         if (err) {
             console.log(err.message);
             res.json({"status" : "error getUserInfo"})
+            return
         }
         res.json({"status" : "ok", "data" : result})
     })
